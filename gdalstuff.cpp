88dd4e77ac30af528e2de1ef5a0207364a5b3e86
Alex Moreno <alexmijowastaken@gmail.com>
2022-06-24 17:48:16 -0500

    Cleaned up gdalstuff.cpp a bit and removed all useless print statements

--- gdalstuff.cpp
+++ gdalstuff.cpp
@@ -8,7 +8,6 @@
 #include <string>
 #include <gdal_priv.h>
 #include <cpl_conv.h>
-//#include "gdalwarper.h"
 #include <stdlib.h>
 #include <cmath>
 
@@ -51,13 +50,9 @@ public:
     // for when object goes out of scope or is removed
     // from memory. 
     ~Geotiff() {
-        // close the Geotiff dataset, free memory for array.  
-        try {
-            GDALClose(geotiffDataset);
-            GDALDestroyDriverManager();
-        } catch (...) {
-            cout << "Failed to destroy a Tiff, I think" << endl;
-        }
+        // close the Geotiff dataset, free memory for array.
+        GDALClose(geotiffDataset);
+        GDALDestroyDriverManager();
     }
 
     const char* GetFileName() {
@@ -230,6 +225,8 @@ public:
 double distance(const double& lat1, const double& lat2, const double& lon1, const double& lon2)
 {
     double req = 6378137.0;
+
+    // alexmijo added code for dealing with equatorial points or identical points
     if (lat1 == lat2 && lon1 == lon2) {
         return 0.0;
     } else if (lat1 == 0.0 && lat2 == 0.0) {
@@ -238,7 +235,6 @@ double distance(const double& lat1, const double& lat2, const double& lon1, cons
         double equatorLength = 2.0 * 3.14159265358979323 * req;
         return fractionOfEquator * equatorLength;
     }
-    
 
     const double latitude_01 = lat1 * M_PI / 180.0;
     const double longitude_01 = lon1 * M_PI / 180.0;
@@ -325,65 +321,6 @@ double distance(const double& lat1, const double& lat2, const double& lon1, cons
     return s;
 }
 
-/*
-double distance(double latp, double latc, double longp, double longc) {
-    constexpr double req = 6378137.0;             //Radius at equator
-    constexpr double flat = 1 / 298.257223563;    //flattening of earth
-    constexpr double rpol = (1 - flat) * req;
-
-    // I had to add this part
-    if (latp == latc && longp == longc) {
-        return 0.0;
-    }
-    else if (latp == 0.0 && latc == 0.0) {
-        double lonDiff = abs(longc - longp);
-        double fractionOfEquator = lonDiff / 360.0;
-        double equatorLength = 2.0 * 3.14159265358979323 * req;
-        return fractionOfEquator * equatorLength;
-    }
-
-    double sin_sigma, cos_sigma, sigma, sin_alpha, cos_sq_alpha, cos2sigma;
-    double C, lam_pre;
-
-    // convert to radians
-    latp = M_PI * latp / 180.0;
-    latc = M_PI * latc / 180.0;
-    longp = M_PI * longp / 180.0;
-    longc = M_PI * longc / 180.0;
-
-    const double u1 = atan((1 - flat) * tan(latc));
-    const double u2 = atan((1 - flat) * tan(latp));
-
-    double lon = longp - longc;
-    double lam = lon;
-    double tol = pow(10., -12.); // iteration tolerance
-    double diff = 1.;
-
-    while (abs(diff) > tol) {
-        sin_sigma = sqrt(pow((cos(u2) * sin(lam)), 2.) + pow(cos(u1) * sin(u2) - sin(u1) * cos(u2) * cos(lam), 2.));
-        cos_sigma = sin(u1) * sin(u2) + cos(u1) * cos(u2) * cos(lam);
-        sigma = atan(sin_sigma / cos_sigma);
-        sin_alpha = (cos(u1) * cos(u2) * sin(lam)) / sin_sigma;
-        cos_sq_alpha = 1 - pow(sin_alpha, 2.);
-        cos2sigma = cos_sigma - ((2 * sin(u1) * sin(u2)) / cos_sq_alpha);
-        C = (flat / 16) * cos_sq_alpha * (4 + flat * (4 - 3 * cos_sq_alpha));
-        lam_pre = lam;
-        lam = lon + (1 - C) * flat * sin_alpha * (sigma + C * sin_sigma * (cos2sigma + C * cos_sigma * (2 * pow(cos2sigma, 2.) - 1)));
-        diff = abs(lam_pre - lam);
-    }
-
-    const double usq = cos_sq_alpha * ((pow(req, 2.) - pow(rpol, 2.)) / pow(rpol, 2.));
-    const double A = 1 + (usq / 16384) * (4096 + usq * (-768 + usq * (320 - 175 * usq)));
-    const double B = (usq / 1024) * (256 + usq * (-128 + usq * (74 - 47 * usq)));
-    const double delta_sig = B * sin_sigma * (cos2sigma + 0.25 * B * (cos_sigma * (-1 + 2 * pow(cos2sigma, 2.)) -
-        (1 / 6) * B * cos2sigma * (-3 + 4 * pow(sin_sigma, 2.)) *
-        (-3 + 4 * pow(cos2sigma, 2.))));
-    const double dis = rpol * A * (sigma - delta_sig);
-    const double azi1 = atan2((cos(u2) * sin(lam)), (cos(u1) * sin(u2) - sin(u1) * cos(u2) * cos(lam)));
-
-    return dis;
-} */
-
 // TODO: make direction an enum
 // Only works for up (0) and down (1)
 int boundingBoxEdge(const int x, const int y, const double radiusM, const int direction, Geotiff& popTiff) {
@@ -394,9 +331,9 @@ int boundingBoxEdge(const int x, const int y, const double radiusM, const int di
 
     int edge = y;
     int edgeOfMap = numRows - 1;
-    // 0 = up, 1 = down
     // Start at center
     int incOrDec; // Added to edge to move one pixel in desired direction
+    // 0 = up, 1 = down
     if (direction == 1) {
         incOrDec = 1; // Down: increasing
     }
@@ -408,13 +345,8 @@ int boundingBoxEdge(const int x, const int y, const double radiusM, const int di
         double lat, lon;
         lat = popTiff.coords(x, edge, 0);
         lon = popTiff.coords(x, edge, 1);
-        //cout << lat << endl;
-        //cout << lon << endl;
-        //cout << cenLat << endl;
-        //cout << cenLon << endl;
-        const double distancee = distance(lat, cenLat, lon, cenLon);
-        //cout << distancee << endl;
-        if (distancee > radiusM) {
+        const double distanceFromCen = distance(lat, cenLat, lon, cenLon);
+        if (distanceFromCen > radiusM) {
             edge -= incOrDec; // Went too far, walk it back
             break;
         }
@@ -444,11 +376,7 @@ int* makeKernel(const int cenX, const int cenY, const double radiusM, Geotiff& p
     const double cenLat = popTiff.coords(cenX, cenY, 0);
     const double cenLon = popTiff.coords(cenX, cenY, 1);
     const int northEdge = boundingBoxEdge(cenX, cenY, radiusM, 0, popTiff);
-    //cout << "inbetween" << endl;
     const int southEdge = boundingBoxEdge(cenX, cenY, radiusM, 1, popTiff);
-    //cout << northEdge << endl;
-    //cout << southEdge << endl;
-    //cout << "here2" << endl;
     const int maxPossibleLength = southEdge - northEdge + 1;
     // A faster way of doing a 2D array of dimensions maxPossibleSize x KERNEL_WIDTH
     // Each row consists of: {westX, eastX, northY, southY} describing a summation table rectangle (so KERNEL_WIDTH
@@ -463,7 +391,7 @@ int* makeKernel(const int cenX, const int cenY, const double radiusM, Geotiff& p
         double lon = popTiff.coords(cenX + horizontalOffset, y, 1);
         if (distance(lat, cenLat, lon, cenLon) > radiusM) {
             if (horizontalOffset == 0) {
-                cout << "Something went wrong!1" << endl;
+                cout << "Something went wrong!1" << endl; // TODO: Probably some better way of logging/displaying this error
             }
             horizontalOffset--;
         }
@@ -491,7 +419,6 @@ int* makeKernel(const int cenX, const int cenY, const double radiusM, Geotiff& p
                 while (true) {
                     y++;
                     if (y > southEdge) {
-                        //cout << "Probably shouldn't get here except for maybe at the poles" << endl;
                         tempKernel[kernelIndex(kernelRow, 3)] = y - cenY - 1; // Rectangle can't extend below south edge
                         break;
                     }
@@ -518,19 +445,14 @@ int* makeKernel(const int cenX, const int cenY, const double radiusM, Geotiff& p
             }
         }
     }
-    //cout << "here3" << endl;
 
     kernelLength = kernelRow + 1; // Visible to the caller
-    //cout << "kernelLength: " << kernelLength << endl;
     // Now that we know the length of kernel, we can construct it using the data in tempKernel and then return it
     int* kernel = new int[kernelLength * KERNEL_WIDTH];
     for (kernelRow = 0; kernelRow < kernelLength; kernelRow++) {
-        //cout << "Row " << kernelRow << ": ";
         for (int kernelCol = 0; kernelCol < KERNEL_WIDTH; kernelCol++) {
             kernel[kernelIndex(kernelRow, kernelCol)] = tempKernel[kernelIndex(kernelRow, kernelCol)];
-            //cout << kernel[kernelIndex(kernelRow, kernelCol)] << " ";
         }
-        //cout << endl;
     }
     delete[] tempKernel;
     return kernel;
@@ -538,18 +460,6 @@ int* makeKernel(const int cenX, const int cenY, const double radiusM, Geotiff& p
 
 // Turn the passed in pop table into a summation table (mutates it)
 void turnIntoSummationTable(double** pop, Geotiff& popTiff) {
-    double sumDDumD = 0;
-    for (int i = 0; i <= 3000; i++) {
-        for (int j = 0; j <= 3000; j++) {
-            if (pop[j][i] < 0) {
-                continue;
-            }
-            sumDDumD += pop[j][i];
-        }
-    }
-    cout << "sumDDumD: " << sumDDumD << endl;
-
-
     const int numRows = popTiff.GetDimensions()[0];
     const int numCols = popTiff.GetDimensions()[1];
     for (int x = 0; x < numCols; x++) {
@@ -571,9 +481,6 @@ void turnIntoSummationTable(double** pop, Geotiff& popTiff) {
             }
         }
     }
-
-    cout << "From table: " << pop[3000][3000] << endl;
-    cout << "From table: " << (long long) pop[numRows - 1][numCols - 1] << endl;
 }
 
 // Returns the total population in the specified rectangle. West and north are 1 pixel outside of the rectangle,
@@ -626,12 +533,7 @@ double popWithinKernel(const int cenX, const int cenY, int* kernel, const int ke
     return totalPop;
 }
 
-int main()
-{
-
-    cout << ((long long) distance(80, 0, -88, 0)) << endl;
-    cout << ((long long)distance(90, 0, -180, 0)) << endl;
-
+int main() {
     // Load population data
     const string popDataFilename = "GHS_POP_E2015_GLOBE_R2019A_4326_30ss_V1_0.tif";
     const string gdpDataFilename = "/mnt/c/Users/Administrator/source/repos/gdalstuff/gdalstuff/gdpPPPdata.tif";
@@ -640,35 +542,29 @@ int main()
     const int numRows = popTiff.GetDimensions()[0];
     const int numCols = popTiff.GetDimensions()[1];
     if (numRows != gdpTiff.GetDimensions()[0] || numCols != gdpTiff.GetDimensions()[1]) {
-        cout << "Uh oh" << endl;
+        cout << "Population tiff and GDP tiff aren't same dimensions." << endl; // TODO: Probably some better way to do this
     }
-
     // The population data as a 2d array. First dimension is reverse lattitude, second dimension is longitude.
     double** pop = popTiff.GetRasterBand(1);
 
-    cout << "loaded pop" << endl;
+    cout << "Loaded population tiff and GDP tiff." << endl;
 
     turnIntoSummationTable(pop, popTiff); // Mutates pop
 
-    cout << "summed" << endl;
+    cout << "Constructed population summation table." << endl;
+
+    //-------------------------------Parameters---------------------------------------------
+    // TODO: Add the ability to use multiple radiuses
+    double radiusKm = 15000;
+
+    const bool smallestPopMode = true;
+    
+    const int printMod = 11; // Print lattitude when the Y index mod this is 0
 
     double upLat = 90;
     double downLat = -90;
     double leftLon = -180;
     double rightLon = 180;
-
-    double radiusKm = 15000; //8423----------------------------------------------------------------------------
-
-    double radiusM = radiusKm * 1000;
-    // Turn lat and lon into indices in the pop data.
-    const int leftX = ((leftLon + 180.0) / 360.0) * numCols;
-    const int rightX = ((rightLon + 180.0) / 360.0) * numCols;
-    const int upY = ((-upLat + 90.0) / 180.0) * numRows;
-    const int downY = ((-downLat + 90.0) / 180.0) * numRows;
-
-    cout << "0.5 location" << endl;
-
-    //double radii[17] = { 0.390625, 0.78125, 1.5625, 3.125, 6.25, 12.5, 25, 50, 100, 200, 400, 800, 1600, 3200, 200, 1000, 2000 };
 
     double smallest = 100000000000;
     double largest = 0;
@@ -677,16 +573,20 @@ int main()
     const int largeStep = 18; //16
     const int xLStep = 64; //64
     const int xXLStep = 256; //256
-    
+    //-------------------------------Parameters-end-----------------------------------------
+
+    double radiusM = radiusKm * 1000;
     int step = smallStep;
-    const bool smallestPopMode = true;
+    // Turn lat and lon into indices in the pop data.
+    const int leftX = ((leftLon + 180.0) / 360.0) * numCols;
+    const int rightX = ((rightLon + 180.0) / 360.0) * numCols;
+    const int upY = ((-upLat + 90.0) / 180.0) * numRows;
+    const int downY = ((-downLat + 90.0) / 180.0) * numRows;
 
     for (int cenY = upY; cenY < downY; cenY += step) {
-        if (cenY % 11 == 0) {
+        if (cenY % printMod == 0) {
             cout << "Current lattitude: " << popTiff.coords(100, cenY, 0) << endl;
         }
-
-        cout << "third location" << endl;
 
         int kernelLength;
         int* kernel = makeKernel(1000, cenY, radiusM, popTiff, kernelLength); // Initializes kernelLength
@@ -695,7 +595,7 @@ int main()
             double popWithinNKilometers = popWithinKernel(cenX, cenY, kernel, kernelLength, pop, popTiff);
             if (smallestPopMode) {
                 if (popWithinNKilometers < smallest) {
-                    cout << "Pop within " << radiusKm << " kilometers of (" << (popTiff.coords(cenX, cenY, 0)) << ", " << popTiff.coords(cenX, cenY, 1) << "): " \
+                    cout << "Population within " << radiusKm << " kilometers of (" << (popTiff.coords(cenX, cenY, 0)) << ", " << popTiff.coords(cenX, cenY, 1) << "): " \
                         << ((long long)popWithinNKilometers) << endl;
                     smallest = popWithinNKilometers;
                 }
@@ -730,7 +630,7 @@ int main()
             }
             else {
                 if (popWithinNKilometers > largest) {
-                    cout << "Pop within " << radiusKm << " kilometers of (" << popTiff.coords(cenX, cenY, 0) << ", " << popTiff.coords(cenX, cenY, 1) << "): " \
+                    cout << "Population within " << radiusKm << " kilometers of (" << popTiff.coords(cenX, cenY, 0) << ", " << popTiff.coords(cenX, cenY, 1) << "): " \
                         << ((long long)popWithinNKilometers) << endl;
                     largest = popWithinNKilometers;
                 }
