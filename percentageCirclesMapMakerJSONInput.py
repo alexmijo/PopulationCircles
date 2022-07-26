bb687717512399530f9e8fcfeeb1f00d50c772d6
Alex Moreno <alexmijowastaken@gmail.com>
2022-07-26 12:06:15 -0500

    Found circles 80 and 81

--- percentageCirclesMapMakerJSONInput.py
+++ percentageCirclesMapMakerJSONInput.py
@@ -2,7 +2,7 @@ import rasterio
 import json
 
 # Between 1 and 100 (inclusive). Must already have been JSONized.
-circleToDraw = 79
+circleToDraw = 81
 
 printDimensions = True
 printProgress = True
