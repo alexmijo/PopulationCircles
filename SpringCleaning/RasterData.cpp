1aef3310e19d8f53d6e8c885aba88864f2ce3886
Alex Moreno <alexmijowastaken@gmail.com>
2023-03-18 09:44:44 -0500

    Improvements

--- SpringCleaning/RasterData.cpp
+++ SpringCleaning/RasterData.cpp
@@ -1,5 +1,6 @@
 #include "Utility.cpp"
 #include <fstream>
+#include <vector>
 
 constexpr int k30ArcSecsPerDegree = 2 * 60;
 constexpr int kNumCols = 360 * k30ArcSecsPerDegree;
@@ -16,7 +17,8 @@ class PixelRectangle {
     int west, east;
     int south, north;
 
-    bool contains(Pixel p) const {
+    // TODO: Is Pixel small enough that pass by value is faster?
+    bool contains(const Pixel &p) const {
         return west <= p.x && p.x <= east && north <= p.y && p.y <= south;
     }
 };
@@ -26,91 +28,71 @@ class PixelRectangle {
 class RasterData {
   public:
     // Returns the sum of the data in the specified rectangle.
-    double sumWithinRectangle(PixelRectangle rect) {
+    // TODO: Is PixelRectangle small enough that pass by value is faster?
+    double sumWithinRectangle(const PixelRectangle &rect) {
         return sumTable[rect.south + 1][rect.east + 1] - sumTable[rect.north][rect.east + 1] -
                sumTable[rect.south + 1][rect.west] + sumTable[rect.north][rect.west];
     }
 
     // Get longitude of the center of the <x>th (0 indexed) column.
-    double lon(int x) const { return ((x + 0.5) / width) * 360.0 - 180.0; }
+    double lon(int x) const { return ((x + 0.5) / m_Width) * 360.0 - 180.0; }
 
-    // Get lattitude of the center of the <y>th (0 indexed) row
-    double lat(int y) const { return ((y + 0.5) / height) * 180.0 - 90.0; }
+    // Get latitude of the center of the <y>th (0 indexed) row
+    double lat(int y) const { return ((y + 0.5) / m_Height) * 180.0 - 90.0; }
 
     // Get the latitude and longitude of the center of a pixel.
-    util::Location pixelToLocation(Pixel p) const { return {lat(p.y), lon(p.x)}; }
+    // TODO: Is Pixel small enough that pass by value is faster?
+    util::Location pixelToLocation(const Pixel &p) const { return {lat(p.y), lon(p.x)}; }
 
     // Get the column containing a longitude.
-    int lonToX(double lon) const { return ((lon + 180.0) / 360.0) * width; }
+    int lonToX(double lon) const { return ((lon + 180.0) / 360.0) * m_Width; }
 
-    // Get the row containing a lattitude.
-    int latToY(double lat) const { return ((lat + 90.0) / 180.0) * height; }
+    // Get the row containing a latitude.
+    int latToY(double lat) const { return ((lat + 90.0) / 180.0) * m_Height; }
 
     // Get the pixel containing a location.
-    Pixel locationToPixel(util::Location loc) const { return {lonToX(loc.lon), latToY(loc.lat)}; }
+    // TODO: Is util::Location small enough that pass by value is faster?
+    Pixel locationToPixel(const util::Location &loc) const {
+        return {lonToX(loc.lon), latToY(loc.lat)};
+    }
 
-    RasterData() {}
-    RasterData(const std::string &sumTableFilename) { loadData(sumTableFilename); }
+    RasterData() = default;
+    explicit RasterData(const std::string &sumTableFilename) { loadData(sumTableFilename); }
 
-    // File specified by <sumTableFilename> must consist of an int for height, and int for
-    //  width, and then height * width doubles representing all of the data in the summation
-    //  table.
+    // File specified by <sumTableFilename> must consist of an int for height, an int for width, and
+    // then height * width doubles representing all of the data in the summation table.
     // Projection must be equirectangular.
-    // TODO: Complete spec
     void loadData(const std::string &sumTableFilename) {
-        std::fstream sumTableFile;
-        sumTableFile.open(sumTableFilename, std::ios::in | std::ios::binary);
-        sumTableFile.read(reinterpret_cast<char *>(&height), sizeof(int));
-        sumTableFile.read(reinterpret_cast<char *>(&width), sizeof(int));
-        // TODO: Either use a unique_pointer to array of unique_pointers to arrays of ints, a vector
-        //  of vectors, or the 1D versions of either of those (with an indexing function) for speed.
-        //  No need for new and delete.
-        sumTable = new double *[height + 1];
-        for (int r = 0; r < height + 1; r++) {
-            sumTable[r] = new double[width + 1];
-            if (r == 0) {
-                // Pad with a row of 0s to the north
-                for (int c = 0; c < width + 1; c++) {
-                    sumTable[0][c] = 0;
-                }
-                continue;
-            }
-            // Pad with a column of 0s to the west
-            sumTable[r][0] = 0;
-            // Reads in an entire row at once
-            sumTableFile.read(reinterpret_cast<char *>(&sumTable[r][1]), sizeof(double) * width);
+        std::ifstream sumTableFile(sumTableFilename, std::ios::in | std::ios::binary);
+        sumTableFile.read(reinterpret_cast<char *>(&m_Height), sizeof(int));
+        sumTableFile.read(reinterpret_cast<char *>(&m_Width), sizeof(int));
+        // The 0.0s will remain only as a column of 0s to the west and a row of 0s to the north.
+        // This is padding for the data which will replace the rest of the zeros, and explains why
+        // sumTable has dimensions one greater than both height and width.
+        sumTable.resize(m_Height + 1, std::vector<double>(width + 1, 0.0));
+        for (int r = 1; r < m_Height + 1; r++) {
+            // Read in an entire row at once
+            sumTableFile.read(reinterpret_cast<char *>(&sumTable[r][1]), sizeof(double) * m_Width);
         }
-        mustFree = true;
+        hasData = true;
     }
 
-    ~RasterData() {
-        if (mustFree) {
-            for (int r = 0; r < height + 1; r++) {
-                delete[] sumTable[r];
-            }
-            delete[] sumTable;
-        }
-    }
+    int getNumRows() const { return m_Height; }
 
-    int getNumRows() const { return height; }
+    int getNumCols() const { return m_Width; }
 
-    int getNumCols() const { return width; }
-
-    bool hasData() const { return mustFree; }
+    bool hasData() const { return hasData; }
 
   private:
     // Dimensions of the original raster data.
-    int height = kNumRows;
-    int width = kNumCols;
+    int m_Height = kNumRows;
+    int m_Width = kNumCols;
     // First index is y (lat), second is x (lon). Matrix style indexing (top to bottom, left to
     //  right). Padded with 0s on the north and west, so it is one row and one column larger than
     //  the actual raster data. However, all methods of this class take indices into the original
     //  raster data (so one off in each dimension from the corresponding sumTable indices).
     // To be extra clear, the dimensions of sumTable are height + 1 by width + 1.
-    // TODO: See if I can or should make this an array of arrays (library arrays). Can't be a 2D C
-    //  array cause it's too large.
-    // TODO: See if I should actually make this a 1D array (both for loading speed and using speed)
-    double **sumTable;
-    // True iff destructor must actually free resources
-    bool mustFree{false};
+    std::vector<std::vector<double>> sumTable;
+    // True iff the data has been loaded.
+    bool m_HasData{false};
 };
