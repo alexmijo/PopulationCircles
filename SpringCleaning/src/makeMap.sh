b7f03db8b3cd43eacceb7127a0297b521372b0b7
Alex Moreno <alexmijowastaken@gmail.com>
2023-07-09 10:29:30 -0500

    Better makeMap.sh

--- SpringCleaning/src/makeMap.sh
+++ SpringCleaning/src/makeMap.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-./MapMaker > thing.ppm
-pnmtopng thing.ppm > /mnt/c/Users/Administrator/Desktop/SpringCleaningMaps/testmap.png
-rm thing.ppm
+./MapMaker | pnmtopng > /mnt/c/Users/Administrator/Desktop/SpringCleaningMaps/testmap.png
